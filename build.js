b8e908aa0018d5c60d6470d6e986d2a4cc8ba7b7
Claycuy <unggaslauncher@gmail.com>
2026-07-01 09:16:30 +0800

    chore: Update build.js

--- build.js
+++ build.js
@@ -18,7 +18,7 @@ async function runBuild() {
         '__VUE_PROD_DEVTOOLS__': 'false',
         '__VUE_PROD_HYDRATION_MISMATCH_DETAILS__': 'false'
       },
-      minify: false,
+      minify: true,
       sourcemap: false,
       target: ['es2020'],
       platform: 'browser',
